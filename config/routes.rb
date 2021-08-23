70532d883e8e16c8f2492f4179442ce0e8f3d579
Otoniel Reyes <otoniel@otonielreyes.com>
2021-08-23 07:50:40 -0400

    Deleted duplicated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,2 @@
 Rails.application.routes.draw do
-  root to: 'site#index'
-  get 'home', to: 'site#index'
-  get 'about', to: 'site#about'
-  get 'products', to: 'site#products'
-  get 'pricing', to: 'site#pricing'
-  get 'contact', to: 'site#contact'
-  get 'feed', to: 'site#feed'
-  get 'privacy_policy', to: 'site#privacy_policy'
-  get 'terms', to: 'site#terms'
-  get 'faqs', to: 'site#faqs'
 end
