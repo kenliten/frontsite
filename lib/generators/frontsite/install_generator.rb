19fdd0d595ac47e293176dff19dcddad90a951a3
Otoniel Reyes <otoniel@otonielreyes.com>
2021-08-23 07:50:27 -0400

    Fixed generator

--- lib/generators/frontsite/install_generator.rb
+++ lib/generators/frontsite/install_generator.rb
@@ -6,32 +6,32 @@ module Frontsite
       source_root File.expand_path('templates', __dir__)
 
       def copy_controller
-        copy_file "controllers/site_controller.rb", "app/controllers/site_controller.rb"
+        copy_file "site_controller.rb", "app/controllers/site_controller.rb"
       end
 
       def copy_helper
-        copy_file "helpers/site_helper.rb", "app/helpers/site_helper.rb"
+        copy_file "site_helper.rb", "app/helpers/site_helper.rb"
       end
 
       def copy_layout
-        copy_file "views/layouts/site.html.erb", "app/views/layouts/site.html.erb"
+        copy_file "site.html.erb", "app/views/layouts/site.html.erb"
       end
 
       def copy_partials
-        copy_file "views/shared/_site_navigation.html.erb", "app/views/shared/_site_navigation.html.erb"
-        copy_file "views/shared/_site_footer.html.erb", "app/views/shared/_site_footer.html.erb"
+        copy_file "_site_navigation.html.erb", "app/views/shared/_site_navigation.html.erb"
+        copy_file "_site_footer.html.erb", "app/views/shared/_site_footer.html.erb"
       end
 
       def copy_views
-        copy_file "views/site/home.html.erb", "app/views/site/home.html.erb"
-        copy_file "views/site/about.html.erb", "app/views/site/about.html.erb"
-        copy_file "views/site/products.html.erb", "app/views/site/products.html.erb"
-        copy_file "views/site/pricing.html.erb", "app/views/site/pricing.html.erb"
-        copy_file "views/site/contact.html.erb", "app/views/site/contact.html.erb"
-        copy_file "views/site/feed.html.erb", "app/views/site/feed.html.erb"
-        copy_file "views/site/privacy_policy.html.erb", "app/views/site/privacy_policy.html.erb"
-        copy_file "views/site/terms.html.erb", "app/views/site/terms.html.erb"
-        copy_file "views/site/faqs.html.erb", "app/views/site/faqs.html.erb"
+        copy_file "home.html.erb", "app/views/site/home.html.erb"
+        copy_file "about.html.erb", "app/views/site/about.html.erb"
+        copy_file "products.html.erb", "app/views/site/products.html.erb"
+        copy_file "pricing.html.erb", "app/views/site/pricing.html.erb"
+        copy_file "contact.html.erb", "app/views/site/contact.html.erb"
+        copy_file "feed.html.erb", "app/views/site/feed.html.erb"
+        copy_file "privacy_policy.html.erb", "app/views/site/privacy_policy.html.erb"
+        copy_file "terms.html.erb", "app/views/site/terms.html.erb"
+        copy_file "faqs.html.erb", "app/views/site/faqs.html.erb"
       end
 
       def copy_styles
